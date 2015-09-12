82adf5a63426ba4225d243487ed32a90ba6fcd99
Pradyumna <pradyumna2905@gmail.com>
2015-09-12 16:18:08 -0700

    Construct splash page

--- agenda.js
+++ agenda.js
@@ -1,4 +1,27 @@
+Agendas = new Mongo.Collection('agendas');
+
 if (Meteor.isClient) {
+
+  Template.body.helpers ({
+    agendas: function() {
+      return Agendas.find();
+    }
+  });
+
+  Template.body.events ({
+    'submit .new-form-agenda': function(event) {
+      var title = event.target.title.value;
+      Agendas.insert({
+        title: title,
+        createdAt: new Date()
+      });
+
+      event.target.title.value = "";
+      return false;
+
+    }
+  });
+
 
 }
 
