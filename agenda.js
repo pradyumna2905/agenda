36010e6a4c387a3640e484c262b56261e681260b
Pradyumna <pradyumna2905@gmail.com>
2015-09-13 18:42:12 -0700

    hide completed agendas

--- agenda.js
+++ agenda.js
@@ -4,7 +4,16 @@ if (Meteor.isClient) {
 
   Template.body.helpers ({
     agendas: function() {
+      if (Session.get('hideCompleted')) {
+        return Agendas.find({checked: {$ne:true}});
+      }
+      else {
       return Agendas.find();
+      }
+    },
+
+    hideCompleted: function() {
+      return Session.get('hideCompleted');
     }
   });
 
@@ -20,14 +29,28 @@ if (Meteor.isClient) {
           event.target.title.value = "";
           return false;
       }
+    },
+
+    'change .hide-completed input': function(event) {
+      Session.set('hideCompleted', event.target.checked);
     }
+
   });
 
-
   Template.agenda.events ({
+    'click .toggle-check': function() {
+      Agendas.update(this._id, {$set: {
+          checked: !this.checked
+        }});
+    },
+
     'click .delete': function() {
       Agendas.remove(this._id);
     }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 
 }
